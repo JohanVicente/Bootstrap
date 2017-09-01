88cf3d9b06c5fe0f4e57761c36eacf65bc33bbb2
johanvicente <yvmolina19@misena.edu.co>
2017-09-01 12:46:02 -0500

    Se hacen actualizaciones

--- client/components/footer/footer.controller.js
+++ client/components/footer/footer.controller.js
@@ -5,9 +5,27 @@
   class FooterController {
     constructor() {
     }
+    formsubmit(){
+      /*if(Math.random() > 0.5){
+        console.log('True');
+      }else{
+        console.log('False');
+      }*/
+      this.el;
+      this.error = Math.random() > 0.5;
+      if(this.error){
+        this.validaColor = "has-success";
+        this.validaColorErr = ["form-control-feedback","glyphicon","glyphicon-ok"];
+        this.el = "<label class='control-label' for='inputSuccess2'> Registro exitoso</label>";
+      }else {
+        this.validaColor = "has-error";
+        this.validaColorErr = ["form-control-feedback","glyphicon","glyphicon-remove"];
+        this.el = "<label class='control-label' for='inputSuccess2'> Registro erroneo</label>";
+      }
+    }
   }
   angular.module('bootstrapApp')
-    .component('footeApp', {
+    .component('footerApp', {
       templateUrl: 'components/footer/footer.html',
       controller: FooterController,
       controllerAs: 'vm'
